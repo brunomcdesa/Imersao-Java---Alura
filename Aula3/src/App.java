ace6eaab86692d0597a68452cc7194f7430e70a6
brunomcdesa <brunotkd68@gmail.com>
2023-03-30 14:01:14 -0300

    Finalizando Aula 3

--- Aula3/src/App.java
+++ Aula3/src/App.java
@@ -1,44 +1,35 @@
 import java.io.InputStream;
-import java.net.URI;
 import java.net.URL;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.net.http.HttpResponse.BodyHandlers;
 import java.util.List;
-import java.util.Map;
 
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
 
         // Fazer uma conexão HTTP e buscar os TOP 250 filmes
-        String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
-        URI endereco = URI.create(url);
-        var client = HttpClient.newHttpClient();
-        var request = HttpRequest.newBuilder(endereco).GET().build();
-        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
-        String body = response.body();
+        //String url ="https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
+       // var extrator = new ExtratorDeConteudoIMDB();
 
-        // Pegar apenas os dados necessários (titulo, poster, classificação)
-        var parser = new JsonParser();
-        List<Map<String, String>> listaDeFilmes = parser.parse(body);
+        String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/NASA-APOD.json";
+        var extrator = new ExtratorDeConteudoNasa();
+        var http = new ClienteHTTp();
+        String json = http.buscaDados(url);
 
         // Exibir e manipular os dados
-        
-        var gerador = new GeradorDeFigurinhas();
-        for (Map<String,String> filme : listaDeFilmes) {
-            
-            String urlImagem = filme.get("image");
-            String titulo = filme.get("title");
-            
-            InputStream inputStream = new URL(urlImagem).openStream();
-            String nomeArquivo = "saida/"+ titulo + ".png";
 
-           
+        List<Conteudo> conteudos = extrator.extrairConteudo(json);
+
+        var gerador = new GeradorDeFigurinhas();
+        for (Conteudo conteudo : conteudos) {
+
+            // String urlImagem = conteudo.get("image");
+
+            InputStream inputStream = new URL(conteudo.getUrlImagem()).openStream();
+            String nomeArquivo = "saida/" + conteudo.getTitulo() + ".png";
+
             gerador.criar(inputStream, nomeArquivo);
 
-            System.out.println(titulo);
+            System.out.println(conteudo.getTitulo());
             System.out.println();
         }
     }
---
+++ Aula3/src/ExtratorDeConteudo.java
@@ -0,0 +1,7 @@
+import java.util.List;
+
+public interface ExtratorDeConteudo {
+
+    List<Conteudo> extrairConteudo(String json);
+    
+}
